6b1d432c56b82e6cb5319d87495453a6ce2882e1
Anuja K U <90404551+anuja2299@users.noreply.github.com>
2024-09-20 21:49:03 +0530

    Add files via upload

---
+++ responsiveLanding.js
@@ -0,0 +1,3 @@
+document.getElementById("donateButton").addEventListener("click", function() {
+    alert("Thank you for your interest in donating blood! Please contact your local blood bank.");
+});
